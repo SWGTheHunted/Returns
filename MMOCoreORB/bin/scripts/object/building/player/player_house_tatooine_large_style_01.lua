8f333766a8b6fd6bbbbbf803b5667e7f3eee440c
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-18 16:29:12 -0400

    Update player_house_tatooine_large_style_01.lua

--- MMOCoreORB/bin/scripts/object/building/player/player_house_tatooine_large_style_01.lua
+++ MMOCoreORB/bin/scripts/object/building/player/player_house_tatooine_large_style_01.lua
@@ -43,6 +43,7 @@
 
 object_building_player_player_house_tatooine_large_style_01 = object_building_player_shared_player_house_tatooine_large_style_01:new {
 	lotSize = 5,
+	 storageCapacity = 1000,
 	baseMaintenanceRate = 50,
 	allowedZones = {"dantooine", "lok", "tatooine"},
 	publicStructure = 0,
