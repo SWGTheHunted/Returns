31e6513cc758928575d010d73f0496d86c87bcfb
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-18 14:00:54 -0400

    Update visibility_manager.lua
    Updated terms

--- MMOCoreORB/bin/scripts/managers/jedi/visibility_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/visibility_manager.lua
@@ -1,5 +1,5 @@
 maxVisibility = 8000      -- Maximum visibility
-termThreshold = 4000      -- Visibility Jedi will show on terminals
+termThreshold = 5000      -- Visibility Jedi will show on terminals
 falloffThreshold = 100    -- Visibility jedi will disappear off terminals
 totalDecayTimeInDays = 1 -- Total time before visibility decays from cap
 tickRateInSeconds = 60*60 -- How often visibility will decay in seconds
