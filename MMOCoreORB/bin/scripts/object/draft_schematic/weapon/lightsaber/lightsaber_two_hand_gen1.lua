195c16652443b742042031c3f49b74540a1cf925
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-19 03:14:19 -0400

    Update lightsaber_two_hand_gen1.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_two_hand_gen1.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_two_hand_gen1.lua
@@ -50,7 +50,8 @@ object_draft_schematic_weapon_lightsaber_lightsaber_two_hand_gen1 = object_draft
    craftingToolTab = 2048, -- (See DraftSchematicObjectTemplate.h)
    complexity = 16, 
    size = 1, 
-
+   factoryCrateType = "object/factory/factory_crate_weapon.iff",
+   
    xpType = "jedi_general", 
    xp = 0, 
 
@@ -66,7 +67,7 @@ object_draft_schematic_weapon_lightsaber_lightsaber_two_hand_gen1 = object_draft
    ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
    ingredientTitleNames = {"emitter_shroud", "primary_crystal", "activator", "handgrip", "focusing_crystals", "power_field_insulator", "energizers"},
    ingredientSlotType = {0, 1, 0, 0, 1, 0, 0},
-   resourceTypes = {"mineral", "object/tangible/component/weapon/lightsaber/lightsaber_refined_crystal_pack.iff", "metal", "chemical", "object/tangible/component/weapon/lightsaber/lightsaber_refined_crystal_pack.iff", "gas", "metal"},
+   resourceTypes = {"mineral", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "metal", "chemical", "object/tangible/component/weapon/lightsaber/shared_lightsaber_refined_crystal_pack.iff", "gas", "metal"},
    resourceQuantities = {20, 1, 19, 28, 1, 30, 20},
    contribution = {100, 100, 100, 100, 100, 100, 100},
 
