3d825c4a1697c2b82b9a9c99147ad65fefb3373b
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-29 06:14:22 -0400

    Reset Buffs Added to MySWG Vendor

--- MMOCoreORB/bin/scripts/screenplays/tasks/naboo/myswg_vendor.lua
+++ MMOCoreORB/bin/scripts/screenplays/tasks/naboo/myswg_vendor.lua
@@ -1058,14 +1058,14 @@ function myswg_vendor_convo_handler:getNextConversationScreen(conversationTempla
                     --createLoot(pInventory, "junk", 1, false)
                     
             
-      --elseif (optionLink == "buff2" and credits < 20000) then
-                    -- Bail if the player doesn’t have enough cash on hand.  
-                    -- Plays a chat box message from the NPC as well as a system message.
-              --        nextConversationScreen = conversation:getScreen("insufficient_funds")
-              --        creature:sendSystemMessage("You have insufficient funds") 
-              --  elseif (optionLink == "buff2" and credits >= 20000) then
-                    -- Take 10,000 credits from the player’s cash on hand and give player a speederbike.
-                  --  creature:subtractCashCredits(20000)
+                elseif (optionLink == "reset_buffs" and credits < 2000) then
+                     Bail if the player doesn’t have enough cash on hand.  
+                     Plays a chat box message from the NPC as well as a system message.
+                      nextConversationScreen = conversation:getScreen("insufficient_funds")
+                      creature:sendSystemMessage("You have insufficient funds") 
+                elseif (optionLink == "reset_buffs" and credits >= 2000) then
+                     Take 2,000 credits from the player’s cash on hand and give player a speederbike.
+                     creature:subtractCashCredits(2000)
 
 									--	CreatureObject(conversingPlayer):enhanceCharacterDocBuff()
 
