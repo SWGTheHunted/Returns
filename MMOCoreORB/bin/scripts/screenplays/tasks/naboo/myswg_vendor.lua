688caad1cea5ffb29fb400bd02c7cb15ee2ad206
Scott L Edwards <108841804+Miphstoe@users.noreply.github.com>
2025-05-29 10:46:31 -0400

    fix for my SWG Vendor

--- MMOCoreORB/bin/scripts/screenplays/tasks/naboo/myswg_vendor.lua
+++ MMOCoreORB/bin/scripts/screenplays/tasks/naboo/myswg_vendor.lua
@@ -1058,12 +1058,12 @@ function myswg_vendor_convo_handler:getNextConversationScreen(conversationTempla
                     --createLoot(pInventory, "junk", 1, false)
                     
             
-                elseif (optionLink == "reset buffs" and credits < 2000) then
+                elseif (optionLink == "reset_buffs" and credits < 2000) then
                      --Bail if the player doesn’t have enough cash on hand.  
                      --Plays a chat box message from the NPC as well as a system message.
                       nextConversationScreen = conversation:getScreen("insufficient_funds")
                       creature:sendSystemMessage("You have insufficient funds") 
-                elseif (optionLink == "reset buffs" and credits >= 2000) then
+                elseif (optionLink == "reset_buffs" and credits >= 2000) then
                      --Take 2,000 credits from the player’s cash on hand and give player a speederbike.
                      creature:subtractCashCredits(2000)
 
