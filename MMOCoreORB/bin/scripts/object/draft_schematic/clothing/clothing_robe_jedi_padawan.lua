e7fe0fdd101206ffe01c96bde9153e4a6decaa40
Frank <misemerfg@gmail.com>
2025-06-01 14:02:48 -0500

    Fix for client side iff

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_robe_jedi_padawan.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_robe_jedi_padawan.lua
@@ -40,7 +40,9 @@
 --gives permission to release a modified version without this exception; 
 --this exception also makes it possible to release a modified version 
 
-object_draft_schematic_clothing_shared_clothing_robe_jedi_padawan = SharedDraftSchematicObjectTemplate:new {}
+object_draft_schematic_clothing_shared_clothing_robe_jedi_padawan = SharedDraftSchematicObjectTemplate:new {
+    clientTemplateFileName = "object/tangible/wearables/robe/robe_jedi_padawan.iff
+}
 
 object_draft_schematic_clothing_clothing_robe_jedi_padawan = object_draft_schematic_clothing_shared_clothing_robe_jedi_padawan:new {
 
